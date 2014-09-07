f3706eb22b5647002229b78ca1ee569ad4c69b66
Pablo Vizcay <pabo.vizcay@gmail.com>
2014-09-07 02:10:02 -0300

    added posibility to pass base url as parameter

--- bin/restful_server.rb
+++ bin/restful_server.rb
@@ -1,12 +1,12 @@
 require 'restful_objects'
 
-if ARGV.first.nil? then
+if ARGV[0].nil? then
   puts 'You must provide the script path to load: "restful_server script.rb"'
   exit(1)
 end
 
-load ARGV.first
+load ARGV[0]
 
-RestfulObjects::DomainModel.current.base_url = 'http://localhost:4567'
+RestfulObjects::DomainModel.current.base_url = ARGV[1] || 'http://localhost:4567'
 RestfulObjects::Server.run!
 
