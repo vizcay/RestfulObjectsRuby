fd5dd9ac6a21579aff20da01660afe5064ac054d
Pablo Vizcay <pablo.vizcay@gmail.com>
2016-09-03 12:28:28 -0300

    refactored RestfulObjects::Service

--- lib/restful_objects/domain_model/mixins/service.rb
+++ lib/restful_objects/domain_model/mixins/service.rb
@@ -1,18 +1,14 @@
-module RestfulObjects
-  module Service
-    include LinkGenerator
+module RestfulObjects::Service
+  include RestfulObjects::LinkGenerator
 
-    def self.included(base)
-      RestfulObjects::DomainModel.current.register_type(base.name)
+  def self.included(base)
+    RestfulObjects::DomainModel.current.register_type(base.name)
+    RestfulObjects::DomainModel.current.register_service(base)
 
-      base.class_eval do
-        extend ObjectMacros
-        include ObjectBase
-        include ObjectActions
-      end
-
-      RestfulObjects::DomainModel.current.register_service(base)
+    base.class_eval do
+      extend RestfulObjects::ObjectMacros
+      include RestfulObjects::ObjectBase
+      include RestfulObjects::ObjectActions
     end
   end
 end
-
