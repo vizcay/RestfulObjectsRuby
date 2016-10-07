7dae2a2adc1f7a61740dcb1500d58dac51c28351
Pablo Vizcay <pablo.vizcay@gmail.com>
2016-10-07 11:59:51 -0300

    refactorized DomainModel::ObjectProperties

--- lib/restful_objects/domain_model/mixins/object_properties.rb
+++ lib/restful_objects/domain_model/mixins/object_properties.rb
@@ -6,17 +6,6 @@ module RestfulObjects::ObjectProperties
     ro_domain_type.properties[name]
   end
 
-  def ro_put_properties_and_get_representation_response(input)
-    properties = JSON.parse(input)
-    properties.each do |name, value|
-      raise 'property not exists' unless ro_domain_type.properties.include?(name)
-      raise 'read-only property' if ro_domain_type.properties[name].read_only
-      ro_set_property_as_json(name, value['value'])
-      on_after_update if respond_to?(:on_after_update)
-    end
-    [HTTP_OK, { 'Content-Type' => ro_content_type_for_object(ro_domain_type.id) }, ro_get_representation(false).to_json]
-  end
-
   def ro_get_property_response(name)
     name     = String(name)
     property = ro_domain_type.properties[name]
@@ -56,6 +45,17 @@ module RestfulObjects::ObjectProperties
     end
 
     [HTTP_OK, { 'Content-Type' => ro_content_type_for_property }, representation.to_json]
+  end
+
+  def ro_put_properties_and_get_representation_response(input)
+    properties = JSON.parse(input)
+    properties.each do |name, value|
+      raise 'property not exists' unless ro_domain_type.properties.include?(name)
+      raise 'read-only property' if ro_domain_type.properties[name].read_only
+      ro_set_property_as_json(name, value['value'])
+      on_after_update if respond_to?(:on_after_update)
+    end
+    [HTTP_OK, { 'Content-Type' => ro_content_type_for_object(ro_domain_type.id) }, ro_get_representation(false).to_json]
   end
 
   def ro_put_property_and_get_response(name, input)
