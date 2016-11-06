44a412ee5e6c012048577a8d1bd0617cb9c2054f
Pablo Vizcay <pablo.vizcay@gmail.com>
2016-11-06 12:12:57 -0300

    worked on specs

--- spec/integration/objects_properties_spec.rb
+++ spec/integration/objects_properties_spec.rb
@@ -1,15 +1,17 @@
 require_relative '../spec_helper'
 
 describe '=> /objects/:type/:instance_id/properties/' do
-  describe '#ro_put_multiple_properties_and_get_response' do
+  describe 'simple properties,' do
     it 'updates simple property' do
       class TestObject
         include RestfulObjects::Object
         property :name, :string
       end
       test_object = TestObject.new
-      response    = test_object.ro_put_multiple_properties_and_get_response({ 'name' => { 'value' => 'john smith' } }.to_json)
-      expect(response.body).to match_json_expression({ 'members' => { 'name' => { 'value' => 'john smith' } } })
+
+      put "/objects/TestObject/#{test_object.ro_instance_id}", { 'name' => { 'value' => 'john smith' } }.to_json
+
+      expect(last_response.body).to match_json_expression({ members: { name: { value: 'john smith' } } })
     end
 
     it 'updates multiple properties' do
@@ -20,14 +22,101 @@ describe '=> /objects/:type/:instance_id/properties/' do
         property :weight, :decimal
       end
       test_object = TestObject.new
-      response    = test_object.ro_put_multiple_properties_and_get_response({ 'name'   => { 'value' => 'john smith' },
-                                                                              'age'    => { 'value' => '29' },
-                                                                              'weight' => { 'value' => '71.5' } }.to_json)
-      expect(response.body).to match_json_expression({ 'members' => {
-                                                         'name'   => { 'value' => 'john smith' },
-                                                         'age'    => { 'value' => 29 },
-                                                         'weight' => { 'value' => 71.5 } }
-                                                     })
+
+      json =  { 'name' => { 'value' => 'john smith' }, 'age' => { 'value' => '29' }, 'weight' => { 'value' => '71.5' } }.to_json
+
+      put "/objects/TestObject/#{test_object.ro_instance_id}", json
+
+      expect(last_response.body).to match_json_expression({
+        members: {
+          name:   { value: 'john smith' },
+          age:    { value: 29 },
+          weight: { value: 71.5 }
+        }
+      })
+    end
+  end
+
+  describe 'reference properties,' do
+    before(:each) do
+      class ReferenceType
+        include RestfulObjects::Object
+      end
+
+      class PropertyRefTest
+        include RestfulObjects::Object
+        property :reference, { object: ReferenceType }
+      end
+
+      @referenced = ReferenceType.new
+      @object     = PropertyRefTest.new
+    end
+
+    it 'gets representation' do
+      @object.reference = @referenced
+
+      get "/objects/PropertyRefTest/#{@object.ro_instance_id}/properties/reference"
+
+      expect(last_response.body).to match_json_expression(
+        { reference:
+          { value:
+            { rel:    'urn:org.restfulobjects:rels/value;property="reference"',
+              href:   "http://localhost/objects/ReferenceType/#{@referenced.object_id}",
+              type:   'application/json;profile="urn:org.restfulobjects:repr-types/object"',
+              method: 'GET',
+              title:  @referenced.ro_title
+            }
+          }
+        }
+      )
+    end
+
+    it 'lists choices' do
+      choices = [ReferenceType.new, ReferenceType.new, ReferenceType.new]
+      @object.define_singleton_method(:reference_choices) { choices }
+
+      get "/objects/PropertyRefTest/#{@object.ro_instance_id}/properties/reference"
+
+      expect(last_response.body).to match_json_expression(
+        { reference:
+          { value: nil,
+            choices: [
+              { rel:    'urn:org.restfulobjects:rels/value;property="reference"',
+                href:   "http://localhost/objects/ReferenceType/#{choices[0].object_id}",
+                type:   'application/json;profile="urn:org.restfulobjects:repr-types/object"',
+                method: 'GET',
+                title:  choices[0].ro_title },
+              { rel:    'urn:org.restfulobjects:rels/value;property="reference"',
+                href:   "http://localhost/objects/ReferenceType/#{choices[1].object_id}",
+                type:   'application/json;profile="urn:org.restfulobjects:repr-types/object"',
+                method: 'GET',
+                title:  choices[1].ro_title },
+              { rel:    'urn:org.restfulobjects:rels/value;property="reference"',
+                href:   "http://localhost/objects/ReferenceType/#{choices[2].object_id}",
+                type:   'application/json;profile="urn:org.restfulobjects:repr-types/object"',
+                method: 'GET',
+                title:  choices[2].ro_title }
+            ]
+          }
+        }
+      )
+    end
+
+    it 'puts representation' do
+      json = { 'value' => { 'href' => @referenced.ro_absolute_url } }.to_json
+      expect(@object.ro_put_property_and_get_response(:reference, json).last).to match_json_expression(
+        { 'reference' =>
+          { 'value' =>
+            { 'rel'    => 'urn:org.restfulobjects:rels/value;property="reference"',
+              'href'   => "http://localhost/objects/ReferenceType/#{@referenced.object_id}",
+              'type'   => 'application/json;profile="urn:org.restfulobjects:repr-types/object"',
+              'method' => 'GET',
+              'title'  => @referenced.ro_title
+            }
+          }
+        }
+      )
+      expect(@object.reference).to eq @referenced
     end
   end
 end
