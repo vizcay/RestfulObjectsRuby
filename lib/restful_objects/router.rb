f829c0f0bb4ea7756d1e9e5066428a6478cb8150
Pablo Vizcay <pablo.vizcay@gmail.com>
2016-11-06 11:22:20 -0300

    changed router loading to autoload

--- lib/restful_objects/router.rb
+++ lib/restful_objects/router.rb
@@ -1,7 +1,6 @@
 module RestfulObjects::Router
+  autoload :Base, 'restful_objects/router/base'
+  autoload :SupportingResources, 'restful_objects/router/supporting_resources'
+  autoload :DomainObjectResources, 'restful_objects/router/domain_object_resources'
+  autoload :DomainTypeResources, 'restful_objects/router/domain_type_resources'
 end
-
-require_relative 'router/supporting_resources'
-require_relative 'router/domain_object_resources'
-require_relative 'router/domain_type_resources'
-require_relative 'router/base'
